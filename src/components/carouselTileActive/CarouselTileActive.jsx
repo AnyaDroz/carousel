58450506032c5f2400a4517fd7ea886571d8639f
AnyaDroz <anya.drozdova89@gmail.com>
2022-10-15 07:47:42 +0100

    map

--- src/components/carouselTileActive/CarouselTileActive.jsx
+++ src/components/carouselTileActive/CarouselTileActive.jsx
@@ -2,43 +2,51 @@ import React from "react";
 import "../carouselTileActive/carousel_tile_active.scss";
 import Squiggle from "../../images/squiggle.svg";
 import Idlerocks from "../../images/idlerocks.png";
+import { tiles } from "../../helpers/config.js";
+
 const CarouselTileActive = () => {
   return (
-    <div className="active-tile-container">
-      <div className="active-tile-container__topbar">
-        <div className="active-tile-container__topbar__eyebrow-container">
-          <h4 className="active-tile-container__topbar__eyebrow-container__eyebrow-one">
-            Hotel
-          </h4>
-          <h4 className="active-tile-container__topbar__eyebrow-container__eyebrow-two">
-            Remote
-          </h4>
-        </div>
+    <div>
+      {tiles.map((item, key) => (
+        <div className="active-tile-container">
+          <div className="active-tile-container__topbar">
+            <div className="active-tile-container__topbar__eyebrow-container">
+              <h4 className="active-tile-container__topbar__eyebrow-container__eyebrow-one">
+                {item.eyebrowOne}
+              </h4>
+              <h4 className="active-tile-container__topbar__eyebrow-container__eyebrow-two">
+                {item.eyebrowTwo}
+              </h4>
+            </div>
 
-        <div className="active-tile-container__topbar__label-container">
-          <img
-            src={Squiggle}
-            alt="squiggle"
-            className="active-tile-container__topbar__label-container__squiggle"
-          />
-          <h4 className="active-tile-container__topbar__label-container__type">
-            stay
-          </h4>
-        </div>
-      </div>
-      {/* <div className="active-tile-container__image-container">
+            <div className="active-tile-container__topbar__label-container">
+              <img
+                src={Squiggle}
+                alt="squiggle"
+                className="active-tile-container__topbar__label-container__squiggle"
+              />
+              <h4 className="active-tile-container__topbar__label-container__type">
+                {item.label}
+              </h4>
+            </div>
+          </div>
+          {/* <div className="active-tile-container__image-container">
         <img
           src={Idlerocks}
           alt="Idlerocks"
           className="active-tile-container__image-container__image"
         />
       </div> */}
-      <div className="active-tile-container__title">
-        <h1 className="active-tile-container__title__header">The idle rocks</h1>
-        <h2 className="active-tile-container__title__subheader">
-          St mawes, cornwall
-        </h2>
-      </div>
+          <div className="active-tile-container__title">
+            <h1 className="active-tile-container__title__header">
+              {item.title}
+            </h1>
+            <h2 className="active-tile-container__title__subheader">
+              {item.location}
+            </h2>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
--- src/helpers/tile-info.js
+++
@@ -1,9 +0,0 @@
-export const tiles = [
-  {
-    label: "stay",
-    title: "the idle rocks",
-    eyebrowOne: "hotel",
-    eyebrowTwo: "remote",
-    location: "St mawes, cornwall",
-  },
-];
